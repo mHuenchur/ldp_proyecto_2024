e101b53158335013faaf872883dfe3c6f33c253a
mHuenchur <102182282+mHuenchur@users.noreply.github.com>
2024-07-02 21:04:20 -0300

    se agregó un toast en la autenticación

--- app/resources/views/autenticacion/index.php
+++ app/resources/views/autenticacion/index.php
@@ -1,6 +1,27 @@
-<div class="card text-center text-bg-light mx-auto p-2" style="width: 500px;">
-    <h3 class="card-header">FORMULARIO DE AUTENTICACION</h3>
+<div class="card text-bg-light mx-auto p-2" style="width: 500px;">
+    <h3 class="card-header text-center">FORMULARIO DE AUTENTICACION</h3>
     <div class="card-body">
-        <button id="btnLogin" class="btn btn-success m-3">Autenticarse</button>
+        <div class="mb-3">
+            <label for="datoUsuario" class="form-label">Usuario</label>
+            <input type="text" class="form-control" id="datoUsuario">
+        </div>
+        <div class="mb-3">
+            <label for="datoClave" class="form-label">Contraseña</label>
+            <input type="password" class="form-control" id="datoClave">
+        </div>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-center">
+            <button id="btnLogin" class="btn btn-success m-3 text-center">Autenticarse</button>
+        </div>
     </div>
+</div>
+
+<div class="toast-container p-3 position-fixed top-0 start-50 translate-middle-x">
+  <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="toast-header">
+      <strong class="me-auto">ATENCION</strong>
+      <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+    </div>
+    <div id="messageConatiner" class="toast-body">
+    </div>
+  </div>
 </div>
--- public/app/js/autenticacion/autenticacionController.js
+++ public/app/js/autenticacion/autenticacionController.js
@@ -1,15 +1,28 @@
 let authController = {
     data: {
+        //usuario: "",
+        //clave: ""
         usuario: "mHuenchur",
         clave: "clave123"
         //usuario: "lMiguel",
         //clave: "12345678"
     },
     login: ()=>{
+        //let formUsuario = document.getElementById("datoUsuario");
+        //let formClave = document.getElementById("datoClave");
+        //authController.data.usuario = formUsuario.value;
+        //authController.data.clave = formClave.value;
+
         authService.login(authController.data)
         .then(response => {
             if(response.error === "" && response.mensaje === "OK"){
                 window.location.href = "inicio/index"
+            }else{
+                const toastLiveExample = document.getElementById('liveToast')
+                const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+                const message = document.getElementById("messageConatiner");
+                message.innerHTML = response.error;
+                toastBootstrap.show()
             }
         })
     }
